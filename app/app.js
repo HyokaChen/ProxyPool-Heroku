fa1a6226ef93d87c19db8d092e392f8a7849c9f8
sunnywangzi <88769439+sunnywangzi@users.noreply.github.com>
2021-11-27 21:52:02 +0800

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,17 @@
+{
+  "name": "ip",
+  "description": "ip",
+  "repository": "https://github.com/HyokaChen/ProxyPool-Heroku",
+  "website": "https://github.com/HyokaChen/ProxyPool-Heroku",
+  "keywords": ["ip", "ip" ],
+  "addons": [
+    {
+      "plan": "heroku-redistogo:nano",
+      "options": {
+        "version": "12"
+      }
+    }
+  ],
+  "logo":"https://image.1msky.cn/2020/09/09/422d658d82732.png",
+  "stack": "heroku-18"
+}
